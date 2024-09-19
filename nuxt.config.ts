2da6cff4c183b228d09cdab48352886a39481854
Ali Guliyev <ali.guliyevoo7@gmail.com>
2024-09-19 10:56:27 +0300

    chore: add @nuxthq/studio module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   // plugins: ['~/plugins/lightGallery.js'],
-  modules: ['nuxt-swiper', '@nuxt/content', "@nuxt/image", "@nuxthub/core"],
+  modules: [
+    'nuxt-swiper',
+    '@nuxt/content',
+    "@nuxt/image",
+    "@nuxthub/core",
+    '@nuxthq/studio'
+  ],
   css: ['./assets/css/tooplate_style.css', "./assets/css/custom.css"],
   // modules: ['@nuxt/ui'],
   components: true,
